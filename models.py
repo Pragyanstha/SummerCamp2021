98acc42b8cfe09cf3c3d7f2f63dd16499e575983
Pragyanstha <pragyanstha@gmail.com>
2021-09-16 13:25:58 +0900

    More batches

--- models.py
+++ models.py
@@ -22,8 +22,93 @@ class MLP(nn.Module):
         x = self.fc2(x)
         return self.droprateout(x)
 
+class matmul(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x1, x2):
+        x = x1@x2
+        return x
+
+def window_partition(x, window_size):
+    """
+    Args:
+        x: (B, H, W, C)
+        window_size (int): window size
+    Returns:
+        windows: (num_windows*B, window_size, window_size, C)
+    """
+    B, H, W, C = x.shape
+    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+    windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
+    return windows
+def window_reverse(windows, window_size, H, W):
+    """
+    Args:
+        windows: (num_windows*B, window_size, window_size, C)
+        window_size (int): Window size
+        H (int): Height of image
+        W (int): Width of image
+    Returns:
+        x: (B, H, W, C)
+    """
+    B = int(windows.shape[0] / (H * W / window_size / window_size))
+    x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
+    return x
 
 class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., window_size=16):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        self.mat = matmul()
+        self.window_size = window_size
+        if self.window_size != 0:
+            self.relative_position_bias_table = nn.Parameter(
+                torch.zeros((2 * window_size - 1) * (2 * window_size - 1), num_heads))  # 2*Wh-1 * 2*Ww-1, nH
+
+            # get pair-wise relative position index for each token inside the window
+            coords_h = torch.arange(window_size)
+            coords_w = torch.arange(window_size)
+            coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
+            coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
+            relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
+            relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
+            relative_coords[:, :, 0] += window_size - 1  # shift to start from 0
+            relative_coords[:, :, 1] += window_size - 1
+            relative_coords[:, :, 0] *= 2 * window_size - 1
+            relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
+            self.register_buffer("relative_position_index", relative_position_index)
+
+            nn.init.trunc_normal_(self.relative_position_bias_table, std=.02)
+        
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+        attn = (self.mat(q, k.transpose(-2, -1))) * self.scale
+        if self.window_size != 0:
+            relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1).clone()].view(
+                self.window_size * self.window_size, self.window_size * self.window_size, -1)  # Wh*Ww,Wh*Ww,nH
+            relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
+            attn = attn + relative_position_bias.unsqueeze(0)
+        
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        x = self.mat(attn, v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class Attention_org(nn.Module):
     def __init__(self, dim, heads=4, attention_dropout=0., proj_dropout=0.):
         super().__init__()
         self.heads = heads
@@ -71,10 +156,10 @@ def UpSampling(x, H, W):
         return x, H, W
 
 class Encoder_Block(nn.Module):
-    def __init__(self, dim, heads, mlp_ratio=4, drop_rate=0.):
+    def __init__(self, dim, heads, mlp_ratio=4, drop_rate=0., window_size=16):
         super().__init__()
         self.ln1 = nn.LayerNorm(dim)
-        self.attn = Attention(dim, heads, drop_rate, drop_rate)
+        self.attn = Attention(dim, num_heads=heads, attn_drop=drop_rate, proj_drop=drop_rate, window_size=window_size)
         self.ln2 = nn.LayerNorm(dim)
         self.mlp = MLP(dim, dim*mlp_ratio, dropout=drop_rate)
 
@@ -87,10 +172,10 @@ class Encoder_Block(nn.Module):
 
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, depth, dim, heads, mlp_ratio=4, drop_rate=0.):
+    def __init__(self, depth, dim, heads, mlp_ratio=4, drop_rate=0., window_size = 16):
         super().__init__()
         self.Encoder_Blocks = nn.ModuleList([
-            Encoder_Block(dim, heads, mlp_ratio, drop_rate)
+            Encoder_Block(dim, heads, mlp_ratio, drop_rate, window_size)
             for i in range(depth)])
 
     def forward(self, x):
@@ -100,7 +185,7 @@ class TransformerEncoder(nn.Module):
 
 class Generator(nn.Module):
     """docstring for Generator"""
-    def __init__(self, latent_dim=1024, depth1=5, depth2=4, depth3=2, initial_size=8, dim=384, heads=4, mlp_ratio=4, drop_rate=0.):#,device=device):
+    def __init__(self, latent_dim=1024, depth1=5, depth2=4, depth3=3, depth4=2, initial_size=8, dim=384, heads=4, mlp_ratio=4, drop_rate=0., window_size = 16):#,device=device):
         super(Generator, self).__init__()
 
         #self.device = device
@@ -109,23 +194,24 @@ class Generator(nn.Module):
         self.depth1 = depth1
         self.depth2 = depth2
         self.depth3 = depth3
+        self.depth4 = depth4
         self.heads = heads
         self.mlp_ratio = mlp_ratio
         self.droprate_rate =drop_rate
-        
+        self.window_size = window_size
         # Noise is 1024 dims
         self.mlp = nn.Linear(latent_dim, (self.initial_size ** 2) * self.dim)
 
         self.positional_embedding_1 = nn.Parameter(torch.zeros(1, (self.initial_size**2), self.dim))
         self.positional_embedding_2 = nn.Parameter(torch.zeros(1, (self.initial_size*2)**2, self.dim//4))
         self.positional_embedding_3 = nn.Parameter(torch.zeros(1, (self.initial_size*4)**2, self.dim//16))
+        self.positional_embedding_4 = nn.Parameter(torch.zeros(1, (self.initial_size*8)**2, self.dim//64))
+        self.TransformerEncoder_encoder1 = TransformerEncoder(depth=self.depth1, dim=self.dim,heads=self.heads, mlp_ratio=self.mlp_ratio, drop_rate=self.droprate_rate, window_size=0)
+        self.TransformerEncoder_encoder2 = TransformerEncoder(depth=self.depth2, dim=self.dim//4, heads=self.heads, mlp_ratio=self.mlp_ratio, drop_rate=self.droprate_rate, window_size=0)
+        self.TransformerEncoder_encoder3 = TransformerEncoder(depth=self.depth3, dim=self.dim//16, heads=self.heads, mlp_ratio=self.mlp_ratio, drop_rate=self.droprate_rate, window_size=window_size)
+        self.TransformerEncoder_encoder4 = TransformerEncoder(depth=self.depth4, dim=self.dim//64, heads=self.heads, mlp_ratio=self.mlp_ratio, drop_rate=self.droprate_rate, window_size=window_size)
 
-        self.TransformerEncoder_encoder1 = TransformerEncoder(depth=self.depth1, dim=self.dim,heads=self.heads, mlp_ratio=self.mlp_ratio, drop_rate=self.droprate_rate)
-        self.TransformerEncoder_encoder2 = TransformerEncoder(depth=self.depth2, dim=self.dim//4, heads=self.heads, mlp_ratio=self.mlp_ratio, drop_rate=self.droprate_rate)
-        self.TransformerEncoder_encoder3 = TransformerEncoder(depth=self.depth3, dim=self.dim//16, heads=self.heads, mlp_ratio=self.mlp_ratio, drop_rate=self.droprate_rate)
-
-
-        self.linear = nn.Sequential(nn.Conv2d(self.dim//16, 3, 1, 1, 0))
+        self.linear = nn.Sequential(nn.Conv2d(self.dim//64, 3, 1, 1, 0))
 
     def forward(self, noise):
 
@@ -139,11 +225,28 @@ class Generator(nn.Module):
         x = x + self.positional_embedding_2
         x = self.TransformerEncoder_encoder2(x)
 
-        x,H,W = UpSampling(x,H,W)
+        x, H, W = UpSampling(x, H, W)
         x = x + self.positional_embedding_3
-
+        B, _, C = x.size()
+        x = x.view(B, H, W, C)
+        x = window_partition(x, self.window_size)
+        x = x.view(-1, self.window_size*self.window_size, C)
         x = self.TransformerEncoder_encoder3(x)
-        x = self.linear(x.permute(0, 2, 1).view(-1, self.dim//16, H, W))
+        x = x.view(-1, self.window_size, self.window_size, C)
+        x = window_reverse(x, self.window_size, H, W).view(B,H*W,C)
+
+
+        x,H,W = UpSampling(x,H,W)
+        x = x + self.positional_embedding_4
+        B, _, C = x.size()
+        x = x.view(B, H, W, C)
+        x = window_partition(x, self.window_size)
+        x = x.view(-1, self.window_size*self.window_size, C)
+        x = self.TransformerEncoder_encoder4(x)
+        x = x.view(-1, self.window_size, self.window_size, C)
+        x = window_reverse(x, self.window_size, H, W).view(B,H*W,C)
+
+        x = self.linear(x.permute(0, 2, 1).reshape((-1, self.dim//64, H, W)))
 
         return x
 
@@ -169,7 +272,7 @@ class Discriminator(nn.Module):
 
         self.droprate = nn.Dropout(p=drop_rate)
         self.TransfomerEncoder = TransformerEncoder(depth, dim, heads,
-                                      mlp_ratio, drop_rate)
+                                      mlp_ratio=mlp_ratio, drop_rate=drop_rate, window_size=0)
         self.norm = nn.LayerNorm(dim)
         self.out = nn.Linear(dim, num_classes)
         self.apply(self._init_weights)
